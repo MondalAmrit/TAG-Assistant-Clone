ba882256b5734e7465ecdb25d99a6ff0aebb7420
Anirudh3167 <77240291+Anirudh3167@users.noreply.github.com>
2024-04-13 23:44:12 +0530

    minor changes made

--- dataset_preperator.py
+++ dataset_preperator.py
@@ -1,8 +1,14 @@
 from Datasets.BasicChat.prepare import get_dataset as basic_chat_data
 from Datasets.WeatherForecast.prepare import get_dataset as weather_data
-import pprint, random
+# from Datasets.GITcommandHandler.prepare import get_dataset as git_data
+import pprint, csv
 dataset = basic_chat_data() + weather_data()
-random.shuffle(dataset)
 
 print(len(dataset),"\n")
 pprint.pprint(dataset[:8])
+
+with open('C://users/Anirudh/Desktop/TAG_dataset.csv','w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerows(dataset)
+
+print('Dataset uploaded to')
