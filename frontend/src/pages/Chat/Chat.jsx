b76908812aa25f6c56358e1f15ec7440fe8f586a
Rishav Raj <rishavraj1707music@gmail.com>
2024-04-19 22:41:00 +0530

    Add conversations state and loader for changing the message dynamically

--- frontend/src/pages/Chat/Chat.jsx
+++ frontend/src/pages/Chat/Chat.jsx
@@ -1,28 +1,71 @@
+import { Fragment, useEffect, useRef, useState } from "react";
 import { Navbar, Input } from "../../components";
-import { messages } from "../../dummyMessages";
 import "./chat.css";
 
+const time = () => {
+  return new Date().toString().split(" ")[4].substring(0, 5);
+};
+
+const conversation = {
+  user: false,
+  msg: "Hi, I am TAG chatbot. How can I assist you?",
+  time: time(),
+};
+
 const Chat = () => {
+  const chatRef = useRef(null);
+  const [messages, setMessages] = useState([conversation]);
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  useEffect(() => {
+    chatRef.current.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
   return (
     <main className="main">
       <Navbar />
       <div className="message-list">
-        {messages.map((message) => (
-          <>
-            <div className="prompt-container">
-              <div className="prompt">
-                <p>{message[0]}</p>
+        {messages.map((message, index) => (
+          <Fragment key={index}>
+            <div
+              className={
+                message.user ? "prompt-container" : "response-container"
+              }
+            >
+              <div className={message.user ? "prompt" : "response"}>
+                <p>{message.msg}</p>
+                <p
+                  style={{
+                    textAlign: "end",
+                    fontSize: "10px",
+                    marginBottom: "-10px",
+                  }}
+                >
+                  {message.time}
+                </p>
               </div>
             </div>
-            <div className="response-container">
-              <div className="response">
-                <p>{message[1]}</p>
-              </div>
-            </div>
-          </>
+          </Fragment>
         ))}
+        {isGenerating && (
+          <p
+            style={{
+              display: "flex",
+              gap: "1.2rem",
+              color: "rgb(10, 180, 183)",
+              width: "20%",
+            }}
+          >
+            Generating<span className="loader"></span>
+          </p>
+        )}
       </div>
-      <Input />
+      <Input
+        time={time}
+        setIsGenerating={setIsGenerating}
+        setMessages={setMessages}
+      />
+      <div ref={chatRef}></div>
     </main>
   );
 };
