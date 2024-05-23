e26db2bfe4e639049623b29cc7d7c3b7e776eff6
Anirudh3167 <77240291+Anirudh3167@users.noreply.github.com>
2024-05-23 18:13:52 +0530

    Just added some command executors on mobile side

--- MobileFrontend/components/CommandExecutor.tsx
+++ MobileFrontend/components/CommandExecutor.tsx
@@ -1,22 +1,22 @@
-import { Linking, Platform } from 'react-native';
+import { Alert, Linking, Platform } from 'react-native';
 
-const execute_command = (cmd: string) => {
-    if (cmd === 'camera') {
-        let cameraAppScheme;
-        if (Platform.OS === 'android') {
-            // Use the appropriate URI scheme for Android
-            cameraAppScheme = 'package://com.android.camera';
-        } else if (Platform.OS === 'ios') {
-            // Use the appropriate URI scheme for iOS
-            cameraAppScheme = 'cameraplus://';
-        } else {
-            // For other platforms, you might handle differently
-            console.log('Unsupported platform');
-            return;
-        }
-
-        Linking.openURL(cameraAppScheme)
-            .catch(err => console.error('An error occurred', err));
+const execute_command = async (cmd: string) => {
+    let action: string = '';
+    if (cmd === 'whatsapp') {
+        action = 'whatsapp://send?text=' + encodeURI('Hi, This message is generated from the NOVA Assistant');
+        // Whatsapp format : whatsapp://send?phone=<encodeURI()>&text=<encodeURI()>
+    } else if (cmd === 'email') {
+        let recipent: string = 'example@email.com'
+        action = 'mailto:'+recipent+'?subject=Subject%20Here&body=Body%20Here';
+    } else if (cmd === 'call') {
+        action = 'tel:+91-1234567890';
+    } else if (cmd === 'sms') {
+        action = 'sms:+91-9315946948';
+    }
+    try {
+        if (action !== '')  await Linking.openURL(action);
+    } catch (e) {
+        console.log('Some Error Occurred');
     }
 }
 
